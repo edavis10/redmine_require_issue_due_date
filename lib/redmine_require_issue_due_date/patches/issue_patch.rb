d629444d1f276825ffcfa630cdd0c8eb837d6acf
Eric Davis <edavis@littlestreamsoftware.com>
2010-03-23 11:30:14 -0700

    [#3803] Added test suite and basic patch for Issue.

---
+++ lib/redmine_require_issue_due_date/patches/issue_patch.rb
@@ -0,0 +1,12 @@
+module RedmineRequireIssueDueDate
+  module Patches
+    module IssuePatch
+
+      def self.included(base)
+        base.class_eval do
+
+        end
+      end
+    end
+  end
+end
---
+++ test/unit/lib/redmine_require_issue_due_date/patches/issue_patch_test.rb
@@ -0,0 +1,4 @@
+require File.dirname(__FILE__) + '/../../../../test_helper'
+
+class RedmineRequireIssueDueDate::Patches::IssuePatchTest < ActiveSupport::TestCase
+end
